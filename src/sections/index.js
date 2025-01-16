90b1161a5cf599218052674965ac3633a8387366
Mzzzz1212 <mahizhsiva@gmail.com>
2025-01-16 16:56:36 +0530

    second commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from "./PopularProducts.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from "./Footer"
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribe,
+    Footer,
+}
